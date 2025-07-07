179a56bce5f787d778f1a777c7a00e8fd3c3a565
RubinaAkther <rubinaakther3454@gmail.com>
2025-07-07 16:34:57 +0600

    Setup tailwind css and added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-export default function (){
-return <div>app</div>
+import Sidebar from "./components/Sidebar";
+
+export default function App() {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <div className="w-full text-white p-4">
+          <h1>Main Content Area</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
